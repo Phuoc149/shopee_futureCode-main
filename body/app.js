fc32add9a2fef3656b49fb5b52ff62ade38c8a61
Phuoc149 <phuocthien956@gmail.com>
2021-08-11 21:30:21 +0700

    chat + skinny banner

--- body/app.js
+++ body/app.js
@@ -2,3 +2,9 @@ $(".carousel").carousel({
   interval: 2000,
 });
 $(".carousel").carousel("prev");
+
+// chat
+function chat() {
+  const c = document.getElementById("chat-main");
+  c.classList.toggle("display");
+}
