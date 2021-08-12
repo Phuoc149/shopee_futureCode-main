6ef371c56403160f953837bce7c939a8ba01e198
Phuoc149 <phuocthien956@gmail.com>
2021-08-12 14:06:34 +0700

    function button

--- HTML/app.js
+++ HTML/app.js
@@ -1,7 +1,7 @@
-$(".carousel").carousel({
-  interval: 2000,
-});
-$(".carousel").carousel("prev");
+// $(".carousel").carousel({
+//   interval: 2000,
+// });
+// $(".carousel").carousel("prev");
 
 // chat
 function chat() {
@@ -9,10 +9,34 @@ function chat() {
   c.classList.toggle("display");
 }
 //
-$(function () {
-  var includes = $("[data-include]");
-  $.each(includes, function () {
-    var file = "views/" + $(this).data("include") + ".html";
-    $(this).load(file);
-  });
-});
+// $(function () {
+//   var includes = $("[data-include]");
+//   $.each(includes, function () {
+//     var file = "views/" + $(this).data("include") + ".html";
+//     $(this).load(file);
+//   });
+// });
+//cart
+let btnAdd = document.getElementById("btn+");
+let money = document.getElementById("p-money");
+let btnMinus = document.getElementById("btn-");
+let total = document.getElementById("total");
+let totalMoney = document.getElementById("total-money");
+let count = 1;
+btnAdd.onclick = function (e) {
+  count++;
+  let value = document.getElementById("inpput");
+  value.value = count;
+  money.textContent = count * 200 + ".000";
+  total.textContent = "Tổng thanh toán ( " + count + " sản phẩm):";
+  totalMoney.textContent = money.textContent;
+};
+
+btnMinus.onclick = function (e) {
+  count--;
+  let value = document.getElementById("inpput");
+  value.value = count;
+  money.textContent = count * 200 + ".000";
+  total.textContent = "Tổng thanh toán (" + count + "sản phẩm):";
+  totalMoney.textContent = money.textContent;
+};
