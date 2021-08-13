710fa267a43794e48b094103b58ca847221ceffe
khoa6430 <khoa6430@gmail.com>
2021-08-13 16:14:05 +0700

    Final Shopee

--- HTML/app.js
+++ HTML/app.js
@@ -8,6 +8,14 @@ function chat() {
   const c = document.getElementById("chat-main");
   c.classList.toggle("display");
 }
+// let btnx = document.getElementsByClassName("btn-close");
+let popup = document.getElementById("popup");
+// btnx.onclick = function (e) {
+//   popup.style.display = "none";
+// };
+function Close() {
+  popup.style.display = "none";
+}
 //
 // $(function () {
 //   var includes = $("[data-include]");
@@ -17,26 +25,26 @@ function chat() {
 //   });
 // });
 //cart
-let btnAdd = document.getElementById("btn+");
+//kho qua ha sufu, chạy rieneng k sao, vào chung là bị. sufu ?
 let money = document.getElementById("p-money");
-let btnMinus = document.getElementById("btn-");
 let total = document.getElementById("total");
 let totalMoney = document.getElementById("total-money");
 let count = 1;
-btnAdd.onclick = function (e) {
+// console.log(popup);
+function Add() {
   count++;
+  console.log(count);
   let value = document.getElementById("inpput");
   value.value = count;
   money.textContent = count * 200 + ".000";
   total.textContent = "Tổng thanh toán ( " + count + " sản phẩm):";
   totalMoney.textContent = money.textContent;
-};
-
-btnMinus.onclick = function (e) {
+}
+function Minus() {
   count--;
   let value = document.getElementById("inpput");
   value.value = count;
   money.textContent = count * 200 + ".000";
   total.textContent = "Tổng thanh toán (" + count + "sản phẩm):";
   totalMoney.textContent = money.textContent;
-};
+}
